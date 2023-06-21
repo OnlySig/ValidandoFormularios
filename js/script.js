b00943149b9a728cb1812dca828266fcd8165b1f
OnlySig <gabriel.castro.pereira11@gmail.com>
2023-06-21 15:15:33 -0300

    validacao idade + 18

--- js/script.js
+++ js/script.js
@@ -1,13 +1,20 @@
 import ehUmCpf from "./validaCpf.js";
+import ehMaiorDeIdade from "./validaIdade.js"
 
 const camposDoFormulario = document.querySelectorAll("[required]")
 
 camposDoFormulario.forEach((campo) => {
     campo.addEventListener("blur", () => verificaCampo(campo));
+    campo.addEventListener("invalid", event => event.preventDefault())
 })
 
 function verificaCampo(campo) {
     if(campo.name == "cpf" && campo.value.length >= 11) {
         ehUmCpf(campo);
     }
+    if(campo.name == "aniversario" && campo.value != '') {
+        ehMaiorDeIdade(campo)
+    }
+
+    console.log(campo.validity)
 }
